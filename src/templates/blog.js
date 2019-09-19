e0a22eb6a2b1f023d462796728d43a08fd00a2e1
davydka <davydka@gmail.com>
2019-09-19 15:06:37 -0700

    Adding mardown html to post content via graphql and graphql query variables.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {graphql} from 'gatsby'
 
 import Layout from '../components/layout'
 
-const Blog = () => {
+export const query = graphql`
+    query (
+      $slug: String!
+    ) {
+      markdownRemark(
+        fields: {
+          slug: {
+            eq: $slug
+          }
+        }
+      ) {
+        frontmatter {
+          title
+          date
+        }
+        html
+        fields {
+          slug
+        }
+      }
+    }
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            yes.
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}} />
         </Layout>
     )
 }
